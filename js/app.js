26eab86074ce5554a00d95a9a2d8276db9aa92d9
maximodn <fornoto68@gmail.com>
2026-07-29 08:17:19 -0300

    Funcion agregar

--- js/app.js
+++ js/app.js
@@ -4,10 +4,19 @@ function mostrarT() {
     const tar = document.createElement("div");
     tar.classList.add("tarea");
     tar.innerHTML = `
-    <textarea name="nueva-tarea" id="n-tarea"></textarea> 
+    <input name="nueva-tarea" id="n-tarea"></input> 
     <button type="submit" id="agregar">Agregar</button>
     `
     t.appendChild(tar);
+
+    const boton = tar.querySelector("#agregar");
+    boton.addEventListener('click', () => agregar());
+}
+
+function agregar() {
+        const input = document.getElementById("n-tarea");
+        const valor = input.value;
+        console.log(valor);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
